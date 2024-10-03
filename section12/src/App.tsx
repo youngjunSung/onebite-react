078fa8927013ccf6e1342c7c5d5ef6b055b34eaa
youngjunSung <seong0814@gmail.com>
2024-10-04 00:59:25 +0900

    feat: 기본 리소스 세팅, scss 적용

--- section12/src/App.tsx
+++ section12/src/App.tsx
@@ -5,6 +5,14 @@ import New from "./pages/New";
 import Diary from "./pages/Diary";
 import Notfound from "./pages/Notfound";
 
+// import emotion1 from "./assets/emotion1.png";
+// import emotion2 from "./assets/emotion2.png";
+// import emotion3 from "./assets/emotion3.png";
+// import emotion4 from "./assets/emotion4.png";
+// import emotion5 from "./assets/emotion5.png";
+
+import { getEmotions } from "./utils/getEmotions";
+
 // "/" : home
 // "/new" : 새로운 일기 생성 페이지
 // "/diary" : 일기 상세 조회 페이지
@@ -16,7 +24,17 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className="flex">
+        <img src="/emotion1.png" alt="" />
+      </div>
+      <div className="flex">
+        <img src={getEmotions(1)} alt="" />
+        <img src={getEmotions(2)} alt="" />
+        <img src={getEmotions(3)} alt="" />
+        <img src={getEmotions(4)} alt="" />
+        <img src={getEmotions(5)} alt="" />
+      </div>
+      <div className="border border-b-amber-500">
         <Link to={"/"}>home</Link>
         <Link to={"/new"}>new</Link>
         <Link to={"/diary"}>diary</Link>
--- section12/src/main.tsx
+++ section12/src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
+import "./index.scss";
 import { BrowserRouter } from "react-router-dom"; // 브라우저의 현재 주소를 저장/감지하는 역할
 
 createRoot(document.getElementById("root")!).render(
