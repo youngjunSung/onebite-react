48595434f82426024a35123a76d9271cb0c7e84b
성영 <youngjunsung@hecto.co.kr>
2024-11-06 17:00:25 +0900

    chore: 상세 작업 중

--- section12/src/components/DiaryItem.tsx
+++ section12/src/components/DiaryItem.tsx
@@ -2,10 +2,11 @@ import Button from "./Button";
 import { getEmotions } from "../utils/getEmotions";
 import { DiaryType } from "../typing/types";
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-type DiaryItemType = Omit<DiaryType, 'id'>
+type DiaryItemType = Omit<DiaryType, "id">;
 
-const DiaryItem = ({id, createdDate, emotionId, content}: DiaryType) => {
+const DiaryItem = ({ id, createdDate, emotionId, content }: DiaryType) => {
   const nav = useNavigate();
   const getEmotionBg = (emotionId: number) => {
     switch (emotionId) {
@@ -25,14 +26,28 @@ const DiaryItem = ({id, createdDate, emotionId, content}: DiaryType) => {
   };
   return (
     <li className="flex items-start mb-[10px] last:mb-0">
-      <div className={`${getEmotionBg(emotionId)} basis-[100px] h-[80px] center mr-[10px] rounded-[6px] shrink-0`}>
-        <img src={getEmotions(emotionId)} alt="" className="object-cover h-full" />
-      </div>
-      <div className="mr-[10px] flex-1 min-w-0">
-        <p className="text-[14px] font-bold">{createdDate}</p>
-        <p className="text-[12px] line-clamp-3">{content}</p>
-      </div>
-      <Button text="수정하기" className="w-fit" onClick={() => nav(`/edit/${id}`)} />
+      <Link to={`/diary/${id}`} className="flex flex-1">
+        <div
+          className={`${getEmotionBg(
+            emotionId
+          )} basis-[100px] h-[80px] center mr-[10px] rounded-[6px] shrink-0`}
+        >
+          <img
+            src={getEmotions(emotionId)}
+            alt=""
+            className="object-cover h-full"
+          />
+        </div>
+        <div className="mr-[10px] flex-1 min-w-0">
+          <p className="text-[14px] font-bold">{createdDate}</p>
+          <p className="text-[12px] line-clamp-3">{content}</p>
+        </div>
+      </Link>
+      <Button
+        text="수정하기"
+        className="w-fit"
+        onClick={() => nav(`/edit/${id}`)}
+      />
     </li>
   );
 };
--- section12/src/pages/Diary.tsx
+++ section12/src/pages/Diary.tsx
@@ -1,13 +1,20 @@
 import { useParams, useSearchParams } from "react-router-dom";
+import { useContext } from "react";
+import { DiaryStateContext } from "../App";
+import { getEmotions } from "../utils/getEmotions";
+import { DiaryType } from "../typing/types";
 
 const Diary = () => {
   const { diary } = useParams();
-  const [params, setParams] = useSearchParams();
-  console.dir(params.get('value'))
+  const diaryList = useContext(DiaryStateContext);
+  const getEditDiary = diaryList?.filter((e) => e.id === Number(diary))[0];
+  console.dir(getEditDiary)
+  const {id, createdDate, emotionId, content} = getEditDiary as DiaryType
   return (
     <>
       <h1>Diary</h1>
-      <p>{diary}</p>
+      <p>{getEditDiary?.content}</p>
+      <img src={getEmotions(emotionId)} alt="" />
     </>
   );
 };
