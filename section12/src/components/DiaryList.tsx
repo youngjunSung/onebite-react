7b9994218e9209a4e440482961798c315f7eeb24
성영 <youngjunsung@hecto.co.kr>
2024-11-08 14:36:21 +0900

    feat: 정렬 기능 추가

--- section12/src/components/DiaryList.tsx
+++ section12/src/components/DiaryList.tsx
@@ -1,20 +1,38 @@
 import Button from "../components/Button";
 import DiaryItem from "./DiaryItem";
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { DiaryStateContext } from "../App";
 import { useNavigate } from "react-router-dom";
 import dayjs from "dayjs";
+import { DiaryType } from "../typing/types";
 
 const DiaryList = ({ yeayMonth }: { yeayMonth: string }) => {
   const nav = useNavigate();
   const diaryList = useContext(DiaryStateContext);
-  console.dir(diaryList)
-  console.log(dayjs("2024-11-07").valueOf())
-  const sortedDiaryList = diaryList?.sort((a, b) => Number(new Date(b.createdDate)) - Number(new Date(a.createdDate)) )
+  const [isLatest, setIsLatest] = useState<boolean>(true);
+  const sortedDiaryList = isLatest
+    ? diaryList?.sort(
+        (a, b) =>
+          Number(new Date(b.createdDate)) - Number(new Date(a.createdDate))
+      )
+    : diaryList?.sort(
+        (a, b) =>
+          Number(new Date(a.createdDate)) - Number(new Date(b.createdDate))
+      );
+  const onChangeSort = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    if (e.target.value === "latest") {
+      setIsLatest(true);
+    } else {
+      setIsLatest(false);
+    }
+  };
   return (
     <div className="p-[12px]">
       <div className="flex items-center mb-[20px]">
-        <select className="h-[38px] pr-[10px] text-[14px] font-[500]">
+        <select
+          className="h-[38px] pr-[10px] text-[14px] font-[500]"
+          onChange={onChangeSort}
+        >
           <option value="latest">최신순</option>
           <option value="oldest">오래된 순</option>
         </select>
